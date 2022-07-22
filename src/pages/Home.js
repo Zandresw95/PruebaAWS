99b6a137bcd7cf72485e65313ca3745c4bad23a9
Zandresw95 <waza1995@gmail.com>
2022-07-22 17:13:07 -0500

    actualizacion lista

---
+++ src/pages/Home.js
@@ -0,0 +1,68 @@
+/*import React, {useEffect, useNavigate} from "react";
+import { Button, Row, Col } from "react-bootstrap";
+import adminCard from "../media/img/AdministracionCard.svg";
+import adopcionCard from "../media/img/AdopcionesCard.svg";
+import donacionCard from "../media/img/DonacionesCard.svg";
+import personaCard from "../media/img/PersonasCard.svg";
+import fundacionesCard from "../media/img/FundacionesCard.svg";
+import apadrinamientoCard from "../media/img/ApadrinamientosCard.svg";
+import "./Home.css";
+
+const Home=()=> {
+
+  return (
+    <div className="home-div">
+      <div>
+        <div className="card">
+          <div className="flex flex-row flex-wrap card-container blue-container">
+            <Col>
+              <Button className="btn-svg" variant="outline-light">
+                <img src={apadrinamientoCard} alt="card-apadrinamiento" />
+              </Button>
+            </Col>
+            <Col>
+              <Button className="btn-svg" variant="outline-light">
+                <img src={adopcionCard} alt="card-adopcion" />
+              </Button>
+            </Col>
+            <Col>
+              <Button className="btn-svg" variant="outline-light">
+                <img src={donacionCard} alt="card-donacion" />
+              </Button>
+            </Col>
+          </div>
+          <Row>
+            <Col>
+              <Button className="btn-svg" variant="outline-light">
+                <img src={personaCard} alt="card-personas" />
+              </Button>
+            </Col>
+            <Col>
+              <Button className="btn-svg" variant="outline-light">
+                <img src={fundacionesCard} alt="card-fundaciones" />
+              </Button>
+            </Col>
+            <Col>
+              <Button className="btn-svg" variant="outline-light">
+                <img src={eventosCard} alt="card-eventos" />
+              </Button>
+            </Col>
+            <Col>
+              <Button className="btn-svg" variant="outline-light">
+                <img src={adminCard} alt="card-administracion" />
+              </Button>
+            </Col>
+            <Col>
+              <Button variant="outline-light" onClick={()=>this.cerrarSesion()}>
+                Cerrar Sesion
+              </Button>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
+*/
