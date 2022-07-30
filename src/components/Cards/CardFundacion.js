d6697e8e08a32fe2f67cd3343e4e1bd72cf5f4ce
Zandresw95 <waza1995@gmail.com>
2022-07-29 19:48:08 -0500

    form

--- src/components/Cards/CardFundacion.js
+++ src/components/Cards/CardFundacion.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
+import Modal from '../generic/Modal';
+import FormDonacion from '../Donacion/FormDonacion';
+import './CardFundacion.css';
+let initialStateModal = {
+    form: false,
+};
 
+let modalTypes = {
+    OPEN_FORM: "OPEN_FORM",
+    CLOSE_USUARIOS: "CLOSE_FORM",
+};
 
 const CardFundacion = ({fundacion,tipo}) => {
-    console.log("lo que llega",tipo)
+    const [fundacionSelected,setFundacionSelected]= useState(fundacion)
+    const [stateModal, dispatchModal] = useReducer(
+        reducerModal,
+        initialStateModal
+    );
+
+
+    function reducerModal(state, action) {
+        switch (action.type) {
+            case modalTypes.OPEN_FORM:
+                return { ...state, form: true };
+            case modalTypes.CLOSE_FORM:
+                return { ...state, form: false };
+            default:
+                break;
+        }
+    }
+
+
+    const abrirForm = (id) => {
+        dispatchModal({ type: modalTypes.OPEN_FORM });
+        setFundacionSelected(id);
+    };
+
+    const cerrarForm = () => {
+        dispatchModal({ type: modalTypes.CLOSE_FORM });
+        setFundacionSelected(0);
+    };
     const header = (
         <div className='flex justify-content-center'>
-            <div className='ico-fundacion-default w-6 h-6'></div>
+            <div className='ico-fundacion-default w-5 h-5'></div>
         </div>
     );
     const footer = (
@@ -15,7 +52,9 @@ const CardFundacion = ({fundacion,tipo}) => {
             {tipo == 'apadrinamiento' ?
                 <Button label="Apadrinar" icon="pi pi-check" className='p-button-info' />
                 : tipo == 'donacion' ?
-                    <Button label="Donar" icon="pi pi-check" className='p-button-help' />
+                    <Button label="Donar" icon="pi pi-check" className='p-button-help' onClick={()=>{
+                        abrirForm(fundacion.id_fundacion);
+                    }}/>
                     : tipo == 'adoptar' ?
                         <Button label="Adoptar" icon="pi pi-check" className="p-button-success" />
                         :
@@ -26,10 +65,19 @@ const CardFundacion = ({fundacion,tipo}) => {
 
     return (
         <div className=''>
-            <Card title={fundacion.nombre_fundacion} style={{ width: '28em' }} footer={footer} header={header}>
+            <Card title={fundacion.nombre_fundacion} className='cardFoundacion' footer={footer} header={header}>
                 <p className="m-0" style={{ lineHeight: '1.5' }}><strong>Telefnono:</strong> {fundacion.telefono_fundacion}</p>
                 <p className="m-0" style={{ lineHeight: '1.5' }}><strong>Dirección:</strong>{fundacion.direccion_fundacion}</p>
             </Card>
+            <Modal activo={stateModal.form} cerrar={cerrarForm}>
+                <FormDonacion
+                    id_fundacion={fundacion.id_fundacion}
+                    cerrar={() => {
+                        cerrarForm();
+                    }}
+                    nombre={fundacion.nombre_fundacion}
+                />
+            </Modal>
         </div>
     );
 }
--- src/pages/Fundaciones.js
+++ src/pages/Fundaciones.js
@@ -45,7 +45,7 @@ function Fundaciones({tipo}) {
                         />
                     )
                 })
-                : "No existen personas"
+                : <></>
             }
         </div>
     );
