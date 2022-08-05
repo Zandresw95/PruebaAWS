af71a19645cde24f41fc10e99270c29a511c8d25
Zandresw95 <waza1995@gmail.com>
2022-08-05 16:40:22 -0500

    cambios en pantallas

--- src/helpers/Dbdata.js
+++ src/helpers/Dbdata.js
@@ -1,5 +1,5 @@
 export const host = "localhost";
 export const port = "4000";
 //export const dominio = "backendapp-env.eba-hym7bbet.us-east-1.elasticbeanstalk.com";
-export const dominio = "http://localhost:4000";
-//export const dominio = "https://api.juntandopatitas.online";
+//export const dominio = "http://localhost:4000";
+export const dominio = "https://api.juntandopatitas.online";
